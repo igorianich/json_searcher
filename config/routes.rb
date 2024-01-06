8eed6daae0c997d343549181b06d19dce4810c9d
igorianich <luzniak9@gmail.com>
2024-01-07 01:12:43 +0200

    Add search controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'search' => 'search#index'
+  post 'search' => 'search#search'
+  root 'search#index'
 end
