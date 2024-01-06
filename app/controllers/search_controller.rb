16bfc73507d79e968d5d872e64f6b42478d963a2
igorianich <luzniak9@gmail.com>
2024-01-07 01:15:51 +0200

    Add actions to controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,11 @@
 class SearchController < ApplicationController
+  def index
+  end
 
+  def search
+    search_query = params[:query]
+    results = JsonFileService.search(search_query)
+
+    render json: results
+  end
 end
